c459d2d3c292d718c9b85a793e4fb63cdba1fc03
kiarash14 <omidbakhsh22@gmail.com>
2016-02-03 16:28:04 +0330

    Update leave_ban.lua

--- plugins/leave_ban.lua
+++ plugins/leave_ban.lua
@@ -4,12 +4,12 @@ local function run(msg, matches)
    local action = msg.action.type 
     if data[tostring(msg.to.id)] then
       if data[tostring(msg.to.id)]['settings'] then
-        if data[tostring(msg.to.id)]['settings']['leave_ban'] then 
-          leave_ban = data[tostring(msg.to.id)]['settings']['leave_ban']
+        if data[tostring(msg.to.id)]['settings']['l_b'] then 
+          leave_ban = data[tostring(msg.to.id)]['settings']['l_b']
         end
       end
     end
-   if action == 'chat_del_user' and not is_momod2(msg.action.user.id) and leave_ban == 'yes' then
+   if action == 'chat_del_user' and not is_momod2(msg.action.user.id) and leave_ban == 'yes🔒' then
      	local user_id = msg.action.user.id
      	local chat_id = msg.to.id
      	ban_user(user_id, chat_id)
